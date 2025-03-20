54eba76bbc386e2a49f7e4a9b42ad3b464d97d85
tyholland <tyronehus@gmail.com>
2025-03-19 21:56:14 -0500

    remove eslint react

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -10,5 +9,4 @@ export default [
   { ignores: [".mode_modules/*"] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ];
