451ed0d55e1b4e1675f412b4ed40853801bb49c3
tyholland <tyronehus@gmail.com>
2025-03-28 21:04:22 -0500

    fix cors issue

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,12 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(
   auth({
